8a27d4d795cd5dc6e135b27bb4b449d8cfe4aefe
Luiz Fernando M Gonçalves <56324956+femelo22@users.noreply.github.com>
2023-03-30 10:23:10 -0300

    feat: start project

---
+++ src/main/java/br/com/lfmelo/ImersaoJavaApplication.java
@@ -0,0 +1,15 @@
+package br.com.lfmelo;
+
+import br.com.lfmelo.aula01.FilmeAPI;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImersaoJavaApplication {
+
+	public static void main(String[] args) {
+		FilmeAPI
+		SpringApplication.run(ImersaoJavaApplication.class, args);
+	}
+
+}
